838f4d558537d31c9decfb20a7ee06de653eb8ea
Maja (they/them) <94635445+elliottm24@users.noreply.github.com>
2022-03-15 18:39:47 -0700

    Adding in playerhand and tile classes

---
+++ app/src/androidTest/java/com/example/a301parte/Tile.java
@@ -0,0 +1,25 @@
+package com.example.a301parte;
+
+/**
+ *@authors Jacob Arnez, Maja Elliott, Dylan Kim, Chase Ohmstede
+ * */
+
+public class Tile {
+    public int color;
+    public int tileNum;
+
+    public Tile(int initColor, int initTileNum) {
+        this.color = initColor;
+        this.tileNum = initTileNum;
+    }
+    /*
+    public Tile(Tile orig) {
+        this.color = orig.color;
+        this.tileNum = orig.tileNum;
+    }
+    */
+    @Override
+    public String toString() {
+        return color + " " + tileNum;
+    }
+}
